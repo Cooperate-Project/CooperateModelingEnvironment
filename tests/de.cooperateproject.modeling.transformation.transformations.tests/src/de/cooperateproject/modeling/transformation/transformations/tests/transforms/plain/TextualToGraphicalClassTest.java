9eb264b0c6ebe293787d0d0f6929aae4c2216211
Stephan Seifermann <seifermann@fzi.de>
2016-08-10 09:28:11 +0200

    Added test for text to graph transformation for class diagrams
    - Refactored existing test cases
    - Extended comparison engine to cope with differences that shall not be
    reported

---
+++ tests/de.cooperateproject.modeling.transformation.transformations.tests/src/de/cooperateproject/modeling/transformation/transformations/tests/transforms/plain/TextualToGraphicalClassTest.java
@@ -0,0 +1,94 @@
+package de.cooperateproject.modeling.transformation.transformations.tests.transforms.plain;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.compare.Diff;
+import org.eclipse.emf.compare.Match;
+import org.eclipse.emf.compare.ReferenceChange;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.gmf.runtime.notation.Connector;
+import org.eclipse.gmf.runtime.notation.NotationPackage;
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.m2m.qvt.oml.ModelExtent;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+import de.cooperateproject.modeling.textual.cls.ClsStandaloneSetup;
+import de.cooperateproject.modeling.textual.cls.cls.ClsPackage;
+import de.cooperateproject.modeling.transformation.transformations.tests.util.MatchValueSideConverter;
+
+public class TextualToGraphicalClassTest extends PlainTransformationTestBase {
+
+	private static final URI TRANSFORMATION_URI = createTransformationURI("Textual_to_Graphical_for_Class.qvto");
+
+	static {
+		ClsPackage.eINSTANCE.eClass();
+		ClsStandaloneSetup.doSetup();
+	}
+
+	@Test
+	public void test() throws Exception {
+		URI sourceModelURI = createResourceModelURI("ClassDiagram.xmi");
+		URI umlModelURI = createResourceModelURI("ClassDiagram.uml");
+		URI resultModelURI = createResourceModelURI("ClassDiagram.notation");
+
+		ModelExtent transformationResult = runTransformation(TRANSFORMATION_URI, sourceModelURI, umlModelURI);
+
+		EObject expected = getRootElement(resultModelURI);
+		EObject actual = transformationResult.getContents().get(0);
+		EcoreUtil.resolveAll(getResourceSet());
+
+		assertModelEquals(expected, actual, this::postProcessDifferences);
+	}
+
+	private Collection<Diff> postProcessDifferences(Collection<Diff> diffs) {
+		List<Diff> result = Lists.newLinkedList();
+
+		LinkedList<ReferenceChange> potentialCorrectReferences = Lists
+				.newLinkedList(diffs.stream().filter(d -> d instanceof ReferenceChange).map(d -> (ReferenceChange) d)
+						.filter(d -> Arrays.asList(NotationPackage.eINSTANCE.getEdge_Source(),
+								NotationPackage.eINSTANCE.getEdge_Target()).contains(d.getReference()))
+						.collect(Collectors.toSet()));
+
+		while (!potentialCorrectReferences.isEmpty()) {
+			ReferenceChange c1 = potentialCorrectReferences.pop();
+			Set<ReferenceChange> candidates = potentialCorrectReferences.stream().filter(c -> c.getMatch() == c1.getMatch()).collect(Collectors.toSet());
+			if (candidates.size() != 1) {
+				continue;
+			}
+			ReferenceChange c2 = candidates.iterator().next();
+			
+			Match parentMatch = c1.getMatch();
+			
+			Connector matchLeft = (Connector)parentMatch.getLeft();
+			View matchLeftSource = matchLeft.getSource();
+			View matchLeftTarget = matchLeft.getTarget();
+			
+			Connector matchRight = (Connector)parentMatch.getRight();
+			View matchRightSource = matchRight.getSource();
+			View matchRightTarget = matchRight.getTarget();
+			
+			MatchValueSideConverter sideConverter = new MatchValueSideConverter();
+			sideConverter.add(parentMatch.getComparison().getMatch(c1.getValue()));
+			sideConverter.add(parentMatch.getComparison().getMatch(c2.getValue()));
+			
+			if (sideConverter.convert(matchLeftSource) == matchRightTarget && sideConverter.convert(matchLeftTarget) == matchRightSource) {
+				potentialCorrectReferences.remove(c2);
+				result.add(c1);
+				result.add(c2);
+			}
+		}
+
+		return result;
+	}
+
+}
