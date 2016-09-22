d3f2420bad8c7fec1c1987768ef6fa4bb2651890
Philipp Faller <faller@fzi.de>
2016-09-22 15:59:20 +0200

    Projects initialize and finalize on startup and on opening and closing

---
+++ bundles/de.cooperateproject.ui/src/de/cooperateproject/ui/nature/StartupBuilder.java
@@ -0,0 +1,28 @@
+package de.cooperateproject.ui.nature;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.ui.IStartup;
+
+public class StartupBuilder implements IStartup {
+
+	private static final Logger LOGGER = Logger.getLogger(StartupBuilder.class);
+	
+	@Override
+	public void earlyStartup() {
+		try {
+			for (IProject project : ResourcesPlugin.getWorkspace().getRoot().getProjects()) {
+				if(NatureUtils.hasCooperateNature(project)){
+					project.build(IncrementalProjectBuilder.CLEAN_BUILD, null);
+				}
+			}
+		} catch (CoreException e) {
+			LOGGER.error("Exception during rebuild on startup", e);
+		}
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(new ProjectOpenedListener());
+	}
+
+}
