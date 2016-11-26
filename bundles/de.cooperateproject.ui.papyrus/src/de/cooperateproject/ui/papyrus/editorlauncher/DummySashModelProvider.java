24f60adc2a11064d0c4f113b2596515696077a7b
Stephan Seifermann <seifermann@fzi.de>
2016-11-26 15:59:03 +0100

    Fixed issue regarding remaining view in Papyrus model set.
    Papyrus stores information about the pages opened in the editor. When
    opening the editor for the same model, the information is read. The
    file, however, contains absolute CDO uris including the checkout number.
    If this number changes, the pages cannot be opened.
    
    This leads to broken editor pages AND a wrong value in the view property
    of the model set. This prohibits cleaning up properly.

---
+++ bundles/de.cooperateproject.ui.papyrus/src/de/cooperateproject/ui/papyrus/editorlauncher/DummySashModelProvider.java
@@ -0,0 +1,42 @@
+package de.cooperateproject.ui.papyrus.editorlauncher;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.papyrus.infra.core.resource.sasheditor.AbstractSashModelProvider;
+
+import com.google.common.collect.Maps;
+
+public class DummySashModelProvider extends AbstractSashModelProvider {
+
+	private static final Logger LOGGER = Logger.getLogger(DummySashModelProvider.class);
+	private final Map<URI, File> tmpFiles = Maps.newHashMap();
+	
+	@Override
+	public URI getSashModelURI(URI userModelURI) {
+		if (!tmpFiles.containsKey(userModelURI)) {
+			String normalizedPath = userModelURI.toString().replaceAll("[^A-Za-z0-9]", "_");
+			String suffix = normalizedPath.substring(Math.max(0, normalizedPath.length() - 50));
+			File tmpFile;
+			try {
+				tmpFile = File.createTempFile("sashmodel", suffix);
+			} catch (IOException e) {
+				LOGGER.error("Could not create temporary file.", e);
+				return null;
+			}
+			tmpFile.deleteOnExit();
+			tmpFiles.put(userModelURI, tmpFile);
+		}
+		return URI.createFileURI(tmpFiles.get(userModelURI).getAbsolutePath());
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+		tmpFiles.values().forEach(File::delete);
+	}
+
+}
