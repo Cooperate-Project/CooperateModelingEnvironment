33733c99b972cf7bb0357de329e5caa8f01ccaac
Stephan Seifermann <seifermann@fzi.de>
2016-12-22 19:10:19 +0100

    #24 Added workaround for duplicated references in interface realization

--- bundles/de.cooperateproject.ui/src/de/cooperateproject/ui/util/CDOTransferUMLFirst.java
+++ bundles/de.cooperateproject.ui/src/de/cooperateproject/ui/util/CDOTransferUMLFirst.java
@@ -9,11 +9,17 @@ public class CDOTransferUMLFirst extends CDOTransfer {
 
 	public CDOTransferUMLFirst(CDOTransferSystem sourceSystem, CDOTransferSystem targetSystem) {
 		super(sourceSystem, targetSystem);
+		
 	}
 
 	@Override
 	protected void performFolder(CDOTransferMapping mapping, IProgressMonitor monitor) {
 		super.performFolder(new CDOTransferMappingUMLFirst(mapping), monitor);
+	}
+
+	@Override
+	protected ModelTransferContext createModelTransferContext() {
+		return new CustomModelTransferContext(this);
 	}
 	
 }
