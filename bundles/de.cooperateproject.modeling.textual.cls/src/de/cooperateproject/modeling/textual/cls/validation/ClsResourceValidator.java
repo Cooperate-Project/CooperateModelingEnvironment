d8480157a5e52123c7761e48e0ce07a395a96578
Stephan Seifermann <seifermann@fzi.de>
2017-01-04 17:14:58 +0100

    #20 Import and some refactoring of issue filtering by @thomcz

--- bundles/de.cooperateproject.modeling.textual.cls/src/de/cooperateproject/modeling/textual/cls/validation/ClsResourceValidator.java
+++ bundles/de.cooperateproject.modeling.textual.cls/src/de/cooperateproject/modeling/textual/cls/validation/ClsResourceValidator.java
@@ -1,6 +1,8 @@
 package de.cooperateproject.modeling.textual.cls.validation;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -11,23 +13,40 @@ import org.eclipse.xtext.validation.CheckMode;
 import org.eclipse.xtext.validation.Issue;
 import org.eclipse.xtext.validation.ResourceValidatorImpl;
 
+import com.google.common.collect.Sets;
+
 public class ClsResourceValidator extends ResourceValidatorImpl {
+
+	private static final Set<String> ISSUE_CODE_BLACKLIST = Sets.newHashSet("org.eclipse.emf.ecore.4");
 
 	@Override
 	public List<Issue> validate(Resource resource, CheckMode mode, CancelIndicator mon) throws OperationCanceledError {
 		List<Issue> issues = super.validate(resource, mode, mon);
 
+		Map<IssueCoordinate, List<Issue>> issueCluster = issues.stream()
+				.collect(Collectors.groupingBy(IssueCoordinate::create));
 		Set<Issue> customIssues = issues.stream().filter(issue -> ClsValidatorIssueId.isContained(issue.getCode()))
 				.collect(Collectors.toSet());
-		issues = issues.stream().filter(
-				i -> customIssues.contains(i) || !customIssues.stream().anyMatch(i2 -> issuesAreForSamePlace(i2, i)))
-				.collect(Collectors.toList());
 
-		return issues;
+		return issueCluster.entrySet().stream().flatMap(g -> filterIssueGroup(g.getKey(), g.getValue(), customIssues).stream())
+				.collect(Collectors.toList());
 	}
 
-	private static boolean issuesAreForSamePlace(Issue i1, Issue i2) {
-		return i1.getOffset().equals(i2.getOffset()) && i1.getLength().equals(i2.getLength());
+	private static Collection<Issue> filterIssueGroup(IssueCoordinate issueCoordinate, Collection<Issue> issues, Collection<Issue> mandatoryIssues) {
+		if (IssueCoordinate.UNKNOWN_ISSUE_COORDINATE.equals(issueCoordinate)) {
+			return issues;
+		}
+		
+		Collection<Issue> filteredIssues = issues.stream().filter(i -> mandatoryIssues.contains(i))
+				.collect(Collectors.toList());
+		if (filteredIssues.isEmpty()) {
+			filteredIssues = issues.stream().filter(i -> !ISSUE_CODE_BLACKLIST.contains(i.getCode()))
+					.collect(Collectors.toList());
+			if (filteredIssues.isEmpty()) {
+				filteredIssues = issues;
+			}
+		}
+		return filteredIssues;
 	}
 
 }
