9f0a4a6f4e94b9fa9c09cca30f303f12ac7bb3ee
Stephan Seifermann <seifermann@fzi.de>
2017-01-26 13:55:08 +0100

    Fixed initialization of ResourceSet in cls scope provider test.

--- tests/de.cooperateproject.modeling.textual.cls.tests/src/de/cooperateproject/modeling/textual/cls/tests/scoping/ClsCooperateSimpleScopeProviderTest.java
+++ tests/de.cooperateproject.modeling.textual.cls.tests/src/de/cooperateproject/modeling/textual/cls/tests/scoping/ClsCooperateSimpleScopeProviderTest.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -19,9 +18,7 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-import org.eclipse.uml2.uml.UMLPackage;
-import org.eclipse.uml2.uml.resource.UMLResource;
+import org.eclipse.uml2.uml.resources.util.UMLResourcesUtil;
 import org.eclipse.xtext.junit4.InjectWith;
 import org.eclipse.xtext.junit4.XtextRunner;
 import org.eclipse.xtext.scoping.IScope;
@@ -46,86 +43,79 @@ import de.cooperateproject.modeling.textual.cls.tests.scoping.util.ClsCustomized
 @InjectWith(ClsCustomizedInjectorProvider.DefaultProvider.class)
 public class ClsCooperateSimpleScopeProviderTest {
 
-	private static String TEST_FOLDER = "testmodels/scoping/";
+    private static String TEST_FOLDER = "testmodels/scoping/";
 
-	@Inject
-	private ClsCooperateSimpleScopeProvider subject;
+    @Inject
+    private ClsCooperateSimpleScopeProvider subject;
 
-	private ResourceSet rs;
+    private ResourceSet rs;
 
-	@BeforeClass
-	public static void init() {
-		ClsStandaloneSetup.doSetup();
-	}
+    @BeforeClass
+    public static void init() {
+        ClsStandaloneSetup.doSetup();
+    }
 
-	@Before
-	public void setup() {
-		rs = createResourceSet();
-	}
+    @Before
+    public void setup() {
+        rs = createResourceSet();
+    }
 
-	@After
-	public void tearDown() {
-		rs.getResources().forEach(Resource::unload);
-		rs.getResources().clear();
-	}
+    @After
+    public void tearDown() {
+        rs.getResources().forEach(Resource::unload);
+        rs.getResources().clear();
+    }
 
-	@Test
-	public void testNestedNormalizing() throws Exception {
-		ClassDiagram cd = loadClassDiagram(rs, "simple.cls");
+    @Test
+    public void testNestedNormalizing() throws Exception {
+        ClassDiagram cd = loadClassDiagram(rs, "simple.cls");
 
-		IScope scope = subject.getScope(cd.getRootPackage().getClassifiers().get(0).getMembers().get(0),
-				ClsPackage.eINSTANCE.getUMLReferencingElement_ReferencedElement());
+        IScope scope = subject.getScope(cd.getRootPackage().getClassifiers().get(0).getMembers().get(0),
+                ClsPackage.eINSTANCE.getUMLReferencingElement_ReferencedElement());
 
-		List<ImportNormalizer> normalizers = getNormalizers(scope);
+        List<ImportNormalizer> normalizers = getNormalizers(scope);
 
-		Set<String> expectedPrefixes = Sets.newHashSet("RootElement", "RootElement.Class1", "RootElement.Class1.someOP", "RootElement.someOP");
-		Set<String> actualPrefixes = normalizers.stream().map(n -> n.getImportedNamespacePrefix().toString())
-				.collect(Collectors.toSet());
-	
-		assertThat(actualPrefixes, is(equalTo(expectedPrefixes)));
-	}
-	
-	@SuppressWarnings("unchecked")
-	private static List<ImportNormalizer> getNormalizers(IScope scope)
-			throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-		assertNotNull(scope);
-		assertThat(scope, is(instanceOf(ImportScope.class)));
+        Set<String> expectedPrefixes = Sets.newHashSet("RootElement", "RootElement.Class1", "RootElement.Class1.someOP",
+                "RootElement.someOP");
+        Set<String> actualPrefixes = normalizers.stream().map(n -> n.getImportedNamespacePrefix().toString())
+                .collect(Collectors.toSet());
 
-		ImportScope importScope = (ImportScope) scope;
-		Field normalizersField = scope.getClass().getDeclaredField("normalizers");
-		normalizersField.setAccessible(true);
+        assertThat(actualPrefixes, is(equalTo(expectedPrefixes)));
+    }
 
-		Object normalizers = normalizersField.get(importScope);		
-		assertThat(normalizers, is(instanceOf(List.class)));
+    @SuppressWarnings("unchecked")
+    private static List<ImportNormalizer> getNormalizers(IScope scope)
+            throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+        assertNotNull(scope);
+        assertThat(scope, is(instanceOf(ImportScope.class)));
 
-		assertThat((List<?>) normalizers, everyItem(is(instanceOf(ImportNormalizer.class))));
-		return (List<ImportNormalizer>) normalizers;
-	}
+        ImportScope importScope = (ImportScope) scope;
+        Field normalizersField = scope.getClass().getDeclaredField("normalizers");
+        normalizersField.setAccessible(true);
 
-	private static ResourceSet createResourceSet() {
-		ResourceSet rs = new ResourceSetImpl();
+        Object normalizers = normalizersField.get(importScope);
+        assertThat(normalizers, is(instanceOf(List.class)));
 
-		org.eclipse.emf.ecore.EPackage.Registry packageRegistry = rs.getPackageRegistry();
-		packageRegistry.replace(UMLPackage.eNS_URI, UMLPackage.eINSTANCE);
+        assertThat((List<?>) normalizers, everyItem(is(instanceOf(ImportNormalizer.class))));
+        return (List<ImportNormalizer>) normalizers;
+    }
 
-		Map<String, Object> extensionFactoryMap = rs.getResourceFactoryRegistry().getExtensionToFactoryMap();
-		extensionFactoryMap.put("xmi", new XMIResourceFactoryImpl());
-		extensionFactoryMap.put(UMLResource.FILE_EXTENSION, UMLResource.Factory.INSTANCE);
+    private static ResourceSet createResourceSet() {
+        ResourceSet rs = new ResourceSetImpl();
+        UMLResourcesUtil.init(rs);
+        return rs;
+    }
 
-		return rs;
-	}
+    private static ClassDiagram loadClassDiagram(ResourceSet rs, String name) throws IOException {
+        Resource r = loadResource(rs, name);
+        return (ClassDiagram) r.getContents().get(0);
+    }
 
-	private static ClassDiagram loadClassDiagram(ResourceSet rs, String name) throws IOException {
-		Resource r = loadResource(rs, name);
-		return (ClassDiagram) r.getContents().get(0);
-	}
-
-	private static Resource loadResource(ResourceSet rs, String name) throws IOException {
-		URI resourceURI = URI.createURI(TEST_FOLDER + name);
-		Resource r = rs.createResource(resourceURI, "cls");
-		r.load(Collections.emptyMap());
-		return r;
-	}
+    private static Resource loadResource(ResourceSet rs, String name) throws IOException {
+        URI resourceURI = URI.createURI(TEST_FOLDER + name);
+        Resource r = rs.createResource(resourceURI, "cls");
+        r.load(Collections.emptyMap());
+        return r;
+    }
 
 }
-
