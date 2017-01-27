eb5cea9675904b916fc7277f18aa3434e5098a69
Stephan Seifermann <seifermann@fzi.de>
2017-01-27 09:45:10 +0100

    #7 Activated two parsing tests, fixed tested implementation.

--- bundles/de.cooperateproject.modeling.textual.cls.metamodel/src-gen/de/cooperateproject/modeling/textual/cls/cls/impl/ImplementationImpl.java
+++ bundles/de.cooperateproject.modeling.textual.cls.metamodel/src-gen/de/cooperateproject/modeling/textual/cls/cls/impl/ImplementationImpl.java
@@ -2,12 +2,11 @@
  */
 package de.cooperateproject.modeling.textual.cls.cls.impl;
 
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.uml2.uml.InterfaceRealization;
+
 import de.cooperateproject.modeling.textual.cls.cls.ClsPackage;
 import de.cooperateproject.modeling.textual.cls.cls.Implementation;
-
-import org.eclipse.emf.ecore.EClass;
-
-import org.eclipse.uml2.uml.InterfaceRealization;
 
 /**
  * <!-- begin-user-doc -->
@@ -17,47 +16,60 @@ import org.eclipse.uml2.uml.InterfaceRealization;
  * The following features are implemented:
  * </p>
  * <ul>
- *   <li>{@link de.cooperateproject.modeling.textual.cls.cls.impl.ImplementationImpl#getReferencedElement <em>Referenced Element</em>}</li>
+ * <li>{@link de.cooperateproject.modeling.textual.cls.cls.impl.ImplementationImpl#getReferencedElement <em>Referenced
+ * Element</em>}</li>
  * </ul>
  *
  * @generated
  */
 public class ImplementationImpl extends TypedConnectorImpl implements Implementation {
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	protected ImplementationImpl() {
-		super();
-	}
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * 
+     * @generated
+     */
+    protected ImplementationImpl() {
+        super();
+    }
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	protected EClass eStaticClass() {
-		return ClsPackage.Literals.IMPLEMENTATION;
-	}
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * 
+     * @generated
+     */
+    @Override
+    protected EClass eStaticClass() {
+        return ClsPackage.Literals.IMPLEMENTATION;
+    }
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public InterfaceRealization getReferencedElement() {
-		org.eclipse.uml2.uml.Type implementor = getLeft();
-		org.eclipse.uml2.uml.Type contract = getRight();
-		
-		if (implementor instanceof org.eclipse.uml2.uml.BehavioredClassifier) {
-			org.eclipse.uml2.uml.BehavioredClassifier implementingClassifier = (org.eclipse.uml2.uml.BehavioredClassifier)implementor;
-			return implementingClassifier.getInterfaceRealizations().stream().filter(r -> r.getContract() == contract).findFirst().orElse(null);
-		}
-		
-		return null;
-	}
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * 
+     * @generated
+     */
+    @Override
+    public InterfaceRealization getReferencedElement() {
+        org.eclipse.uml2.uml.Type implementor = getLeft();
+        org.eclipse.uml2.uml.Type contract = getRight();
 
-} //ImplementationImpl
+        if (implementor instanceof org.eclipse.uml2.uml.BehavioredClassifier) {
+            org.eclipse.uml2.uml.BehavioredClassifier implementingClassifier = (org.eclipse.uml2.uml.BehavioredClassifier) implementor;
+            return implementingClassifier.getInterfaceRealizations().stream().filter(r -> r.getContract() == contract)
+                    .findFirst().orElse(null);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Object eGet(int featureID, boolean resolve, boolean coreType) {
+        if (featureID == ClsPackage.IMPLEMENTATION__REFERENCED_ELEMENT) {
+            return getReferencedElement();
+        }
+        return super.eGet(featureID, resolve, coreType);
+    }
+
+} // ImplementationImpl
