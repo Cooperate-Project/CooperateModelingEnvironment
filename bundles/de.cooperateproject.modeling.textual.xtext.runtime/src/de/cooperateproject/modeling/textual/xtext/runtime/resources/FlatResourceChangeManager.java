2cbc7e191d10f252864340f5f7236a5fdd47027c
Stephan Seifermann <seifermann@fzi.de>
2017-01-30 12:00:30 +0100

    Bug fix of FlatResourceChangeManager

--- bundles/de.cooperateproject.modeling.textual.xtext.runtime/src/de/cooperateproject/modeling/textual/xtext/runtime/resources/FlatResourceChangeManager.java
+++ bundles/de.cooperateproject.modeling.textual.xtext.runtime/src/de/cooperateproject/modeling/textual/xtext/runtime/resources/FlatResourceChangeManager.java
@@ -19,107 +19,110 @@ import de.cooperateproject.modeling.textual.xtext.runtime.resources.observer.Res
 
 public abstract class FlatResourceChangeManager extends ResourceChangeManagerBase {
 
-	private class ResourceSetAdapter extends AdapterImpl {
-		
-		@Override
-		public void notifyChanged(Notification notification) {
-			int notificationType = notification.getEventType();
-			if ((notificationType & (Notification.ADD | Notification.ADD_MANY)) != 0) {
-				handleChange(notification.getNewValue(), this::handleAddedResource);
-			} else if ((notificationType & (Notification.REMOVE | Notification.REMOVE_MANY)) != 0) {
-				handleChange(notification.getOldValue(), this::handleRemovedResource);
-			}
-		}
-		
-		private void handleChange(Object value, Consumer<Resource> consumer) {
-			Collection<Resource> resources = null;
-			if (value instanceof Collection) {
-				resources = ((Collection<?>)value).stream().filter(e -> e != null && e instanceof Resource).map(e -> (Resource)e).collect(Collectors.toList());
-			} else if (value instanceof Resource) {
-				resources = Arrays.asList((Resource)value);
-			}
-			if (resources != null) {
-				resources.forEach(consumer);
-			}
-		}
-		
-		private void handleAddedResource(Resource r) {
-			registerAdapter(r);
-		}
-		
-		private void handleRemovedResource(Resource r) {
-			unregisterAdapter(r);
-		}
+    private class ResourceSetAdapter extends AdapterImpl {
 
-	}
-	
-	private final Collection<IResourceChangeObserver> resourceObservers = Sets.newHashSet();
-	private ResourceSetAdapter resourceSetObserver;
-	
-	public FlatResourceChangeManager(PreparedResourceListener...listeners) {
-		super(listeners);
-	}
+        @Override
+        public void notifyChanged(Notification notification) {
+            int notificationType = notification.getEventType();
+            if ((notificationType & (Notification.ADD | Notification.ADD_MANY)) != 0) {
+                handleChange(notification.getNewValue(), this::handleAddedResource);
+            } else if ((notificationType & (Notification.REMOVE | Notification.REMOVE_MANY)) != 0) {
+                handleChange(notification.getOldValue(), this::handleRemovedResource);
+            }
+        }
 
-	@Override
-	protected void initForNewResourceSet(ResourceSet rs) {
-		registerAdapters(rs);
-	}
+        private void handleChange(Object value, Consumer<Resource> consumer) {
+            Collection<Resource> resources = null;
+            if (value instanceof Collection) {
+                resources = ((Collection<?>) value).stream().filter(e -> e != null && e instanceof Resource)
+                        .map(e -> (Resource) e).collect(Collectors.toList());
+            } else if (value instanceof Resource) {
+                resources = Arrays.asList((Resource) value);
+            }
+            if (resources != null) {
+                resources.forEach(consumer);
+            }
+        }
 
-	@Override
-	protected void cleanupForOldResourceSet(ResourceSet rs) {
-		unregisterAdapters(rs);
-	}
+        private void handleAddedResource(Resource r) {
+            registerAdapter(r);
+        }
 
-	private void registerAdapters(ResourceSet rs) {
-		Validate.notNull(rs);
-		
-		registerAdapter(rs);
-		rs.getResources().stream().forEach(this::registerAdapter);		
-	}
-	
-	private void registerAdapter(ResourceSet rs) {
-		Validate.notNull(rs);
-		
-		resourceSetObserver = new ResourceSetAdapter();
-		rs.eAdapters().add(resourceSetObserver);		
-	}
-	
-	private void registerAdapter(Resource r) {
-		Validate.notNull(r);
-		
-		Optional<IResourceChangeObserver> adapter = createResourceAdapter(r);
-		if (adapter.isPresent()) {
-			resourceObservers.add(adapter.get());
-		}
-		fireResourceProcessed(r, PreparationReason.DISCOVERED);
-	}
-	
-	private Optional<IResourceChangeObserver> createResourceAdapter(Resource r) {
-		Validate.notNull(r);
-		
-		return ResourceChangeObserverFactory.create(r, this::handleChangedResourceFiltered);
-	}
+        private void handleRemovedResource(Resource r) {
+            unregisterAdapter(r);
+        }
 
-	private void unregisterAdapters(ResourceSet rs) {
-		Validate.notNull(rs);
-		
-		unregisterAdapter(rs);
-		rs.getResources().iterator().forEachRemaining(this::unregisterAdapter);
-	}
-	
-	private void unregisterAdapter(ResourceSet rs) {
-		Validate.notNull(rs);
-		
-		rs.eAdapters().removeIf(a -> a == resourceSetObserver);
-		resourceSetObserver = null;
-	}
-	
-	private void unregisterAdapter(Resource r) {
-		Validate.notNull(r);
-		
-		resourceObservers.stream().filter(o -> o.getObservedResource() == r).forEach(o -> o.close());
-		resourceObservers.removeIf(o -> o.getObservedResource() == r);
-		fireResourceProcessed(r, PreparationReason.DISCARDED);
-	}
+    }
+
+    private final Collection<IResourceChangeObserver> resourceObservers = Sets.newHashSet();
+    private ResourceSetAdapter resourceSetObserver;
+
+    public FlatResourceChangeManager(PreparedResourceListener... listeners) {
+        super(listeners);
+    }
+
+    @Override
+    protected void initForNewResourceSet(ResourceSet rs) {
+        registerAdapters(rs);
+    }
+
+    @Override
+    protected void cleanupForOldResourceSet(ResourceSet rs) {
+        unregisterAdapters(rs);
+    }
+
+    private void registerAdapters(ResourceSet rs) {
+        Validate.notNull(rs);
+
+        registerAdapter(rs);
+
+        Collection<Resource> resourcesToProcess = Sets.newHashSet(rs.getResources());
+        resourcesToProcess.stream().forEach(this::registerAdapter);
+    }
+
+    private void registerAdapter(ResourceSet rs) {
+        Validate.notNull(rs);
+
+        resourceSetObserver = new ResourceSetAdapter();
+        rs.eAdapters().add(resourceSetObserver);
+    }
+
+    private void registerAdapter(Resource r) {
+        Validate.notNull(r);
+
+        Optional<IResourceChangeObserver> adapter = createResourceAdapter(r);
+        if (adapter.isPresent()) {
+            resourceObservers.add(adapter.get());
+        }
+        fireResourceProcessed(r, PreparationReason.DISCOVERED);
+    }
+
+    private Optional<IResourceChangeObserver> createResourceAdapter(Resource r) {
+        Validate.notNull(r);
+
+        return ResourceChangeObserverFactory.create(r, this::handleChangedResourceFiltered);
+    }
+
+    private void unregisterAdapters(ResourceSet rs) {
+        Validate.notNull(rs);
+
+        unregisterAdapter(rs);
+        rs.getResources().iterator().forEachRemaining(this::unregisterAdapter);
+    }
+
+    private void unregisterAdapter(ResourceSet rs) {
+        Validate.notNull(rs);
+
+        rs.eAdapters().removeIf(a -> a == resourceSetObserver);
+        resourceSetObserver = null;
+    }
+
+    private void unregisterAdapter(Resource r) {
+        Validate.notNull(r);
+
+        resourceObservers.stream().filter(o -> o.getObservedResource() == r).forEach(o -> o.close());
+        resourceObservers.removeIf(o -> o.getObservedResource() == r);
+        fireResourceProcessed(r, PreparationReason.DISCARDED);
+    }
 
 }
