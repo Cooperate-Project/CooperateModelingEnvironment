de71bd25b62bd187cc9e007dafb38a957b3b04f1
Stephan Seifermann <seifermann@fzi.de>
2017-02-26 17:14:08 +0100

    Removed some warnings.

--- bundles/de.cooperateproject.modeling.transformation.transformations/src/de/cooperateproject/modeling/transformation/transformations/impl/QVTOResource.java
+++ bundles/de.cooperateproject.modeling.transformation.transformations/src/de/cooperateproject/modeling/transformation/transformations/impl/QVTOResource.java
@@ -25,154 +25,156 @@ import org.eclipse.m2m.internal.qvt.oml.compiler.UnitProxy;
 import org.eclipse.m2m.internal.qvt.oml.compiler.UnitResolverFactory;
 import org.eclipse.m2m.internal.qvt.oml.expressions.OperationalTransformation;
 
+@SuppressWarnings("restriction")
 public class QVTOResource implements Resource {
 
-	private final URI uri;
-	private final CompiledUnit compiledUnit;
-	
-	public QVTOResource(URI qvtoResource, EPackage.Registry packageRegistry) throws IOException {
-		this.uri = qvtoResource;
-		UnitProxy unit = UnitResolverFactory.Registry.INSTANCE.getUnit(qvtoResource);
-		QVTOCompiler compiler = QVTOCompiler.createCompiler(packageRegistry);
-		try {
-			compiledUnit = compiler.compile(unit, null, BasicMonitor.toMonitor(new NullProgressMonitor()));
-		} catch (MdaException e) {
-			throw new IOException("Could not load and compile resource.", e);
-		}
-	}
-	
-	public OperationalTransformation getFirstTransformation() {
-		return compiledUnit.getModules().stream().filter(m -> m instanceof OperationalTransformation).map(m -> (OperationalTransformation)m).findAny().get();
-	}
+    private final URI uri;
+    private final CompiledUnit compiledUnit;
 
-	@Override
-	public EList<Adapter> eAdapters() {
-		throw new UnsupportedOperationException();
-	}
+    public QVTOResource(URI qvtoResource, EPackage.Registry packageRegistry) throws IOException {
+        this.uri = qvtoResource;
+        UnitProxy unit = UnitResolverFactory.Registry.INSTANCE.getUnit(qvtoResource);
+        QVTOCompiler compiler = QVTOCompiler.createCompiler(packageRegistry);
+        try {
+            compiledUnit = compiler.compile(unit, null, BasicMonitor.toMonitor(new NullProgressMonitor()));
+        } catch (MdaException e) {
+            throw new IOException("Could not load and compile resource.", e);
+        }
+    }
 
-	@Override
-	public boolean eDeliver() {
-		throw new UnsupportedOperationException();
-	}
+    public OperationalTransformation getFirstTransformation() {
+        return compiledUnit.getModules().stream().filter(m -> m instanceof OperationalTransformation)
+                .map(m -> (OperationalTransformation) m).findAny().get();
+    }
 
-	@Override
-	public void eSetDeliver(boolean deliver) {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public EList<Adapter> eAdapters() {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public void eNotify(Notification notification) {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public boolean eDeliver() {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public ResourceSet getResourceSet() {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public void eSetDeliver(boolean deliver) {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public URI getURI() {
-		return uri;
-	}
+    @Override
+    public void eNotify(Notification notification) {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public void setURI(URI uri) {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public ResourceSet getResourceSet() {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public long getTimeStamp() {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public URI getURI() {
+        return uri;
+    }
 
-	@Override
-	public void setTimeStamp(long timeStamp) {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public void setURI(URI uri) {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public EList<EObject> getContents() {
-		return new BasicEList<EObject>(Arrays.asList(getFirstTransformation()));
-	}
+    @Override
+    public long getTimeStamp() {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public TreeIterator<EObject> getAllContents() {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public void setTimeStamp(long timeStamp) {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public String getURIFragment(EObject eObject) {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public EList<EObject> getContents() {
+        return new BasicEList<EObject>(Arrays.asList(getFirstTransformation()));
+    }
 
-	@Override
-	public EObject getEObject(String uriFragment) {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public TreeIterator<EObject> getAllContents() {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public void save(Map<?, ?> options) throws IOException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public String getURIFragment(EObject eObject) {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public void load(Map<?, ?> options) throws IOException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public EObject getEObject(String uriFragment) {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public void save(OutputStream outputStream, Map<?, ?> options) throws IOException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public void save(Map<?, ?> options) throws IOException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public void load(InputStream inputStream, Map<?, ?> options) throws IOException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public void load(Map<?, ?> options) throws IOException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public boolean isTrackingModification() {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public void save(OutputStream outputStream, Map<?, ?> options) throws IOException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public void setTrackingModification(boolean isTrackingModification) {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public void load(InputStream inputStream, Map<?, ?> options) throws IOException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public boolean isModified() {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public boolean isTrackingModification() {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public void setModified(boolean isModified) {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public void setTrackingModification(boolean isTrackingModification) {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public boolean isLoaded() {
-		return true;
-	}
+    @Override
+    public boolean isModified() {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public void unload() {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public void setModified(boolean isModified) {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public void delete(Map<?, ?> options) throws IOException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public boolean isLoaded() {
+        return true;
+    }
 
-	@Override
-	public EList<Diagnostic> getErrors() {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public void unload() {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public EList<Diagnostic> getWarnings() {
-		throw new UnsupportedOperationException();
-	}
-	
+    @Override
+    public void delete(Map<?, ?> options) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public EList<Diagnostic> getErrors() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public EList<Diagnostic> getWarnings() {
+        throw new UnsupportedOperationException();
+    }
+
 }
--- bundles/de.cooperateproject.ui/src/de/cooperateproject/ui/wizards/modelnew/ModelAndDiagramSelectionComposite.java
+++ bundles/de.cooperateproject.ui/src/de/cooperateproject/ui/wizards/modelnew/ModelAndDiagramSelectionComposite.java
@@ -24,135 +24,148 @@ import org.eclipse.swt.widgets.Tree;
 import de.cooperateproject.ui.wizards.modelnew.AtomicModelNameProcessor.ModelName;
 
 public class ModelAndDiagramSelectionComposite extends Composite {
-	private final DataBindingContext m_bindingContext;
-	private final Text textModel;
-	private final Text textDiagramName;
-	private final TreeViewer treeViewer;
+    private final DataBindingContext m_bindingContext;
+    private final Text textModel;
+    private final Text textDiagramName;
+    private final TreeViewer treeViewer;
 
-	private final SelectedNamesDTO dto;
-	private final IChangeListener validatorStatusListener;
-	private AggregateValidationStatus aggregatedStatus;
+    private final SelectedNamesDTO dto;
+    private final IChangeListener validatorStatusListener;
+    private AggregateValidationStatus aggregatedStatus;
 
-	/**
-	 * Create the composite.
-	 * @param parent
-	 * @param style
-	 */
-	public ModelAndDiagramSelectionComposite(Composite parent, int style, SelectedNamesDTO dto, IChangeListener validatorStatusListener) {
-		super(parent, style);
-		this.dto = dto;
-		this.validatorStatusListener = validatorStatusListener;
-		setLayout(new GridLayout(2, false));
-		
-		Label lblEnterOrSelect = new Label(this, SWT.NONE);
-		lblEnterOrSelect.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
-		lblEnterOrSelect.setText("Enter or select the model:");
-		
-		textModel = new Text(this, SWT.BORDER);
-		textModel.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
-		
-		treeViewer = new TreeViewer(this, SWT.BORDER);
-		Tree tree = treeViewer.getTree();
-		tree.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
-		treeViewer.setLabelProvider(new ModelLabelProvider());
-		treeViewer.setContentProvider(new ModelContentProvider());
-		treeViewer.setInput(dto);
-		
-		Label lblDiagramName = new Label(this, SWT.NONE);
-		lblDiagramName.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
-		lblDiagramName.setText("Diagram Name:");
-		
-		textDiagramName = new Text(this, SWT.BORDER);
-		textDiagramName.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		m_bindingContext = initDataBindings();
-		initCustomDataBindings();
-		initCustomListeners();
-	}
+    /**
+     * Create the composite.
+     * 
+     * @param parent
+     * @param style
+     */
+    public ModelAndDiagramSelectionComposite(Composite parent, int style, SelectedNamesDTO dto,
+            IChangeListener validatorStatusListener) {
+        super(parent, style);
+        this.dto = dto;
+        this.validatorStatusListener = validatorStatusListener;
+        setLayout(new GridLayout(2, false));
 
-	@Override
-	protected void checkSubclass() {
-		// Disable the check that prevents subclassing of SWT components
-	}
-	protected DataBindingContext initDataBindings() {
-		DataBindingContext bindingContext = new DataBindingContext();
-		//
-		IObservableValue observeSingleSelectionTreeViewer = ViewerProperties.singleSelection().observe(treeViewer);
-		IObservableValue textTextModelObserveValue = PojoProperties.value("text").observe(textModel);
-		UpdateValueStrategy strategy_1 = new UpdateValueStrategy();
-		strategy_1.setConverter(new ModelNodeConverter());
-		bindingContext.bindValue(observeSingleSelectionTreeViewer, textTextModelObserveValue, strategy_1, new UpdateValueStrategy(UpdateValueStrategy.POLICY_NEVER));
-		//
-		return bindingContext;
-	}
-	
+        Label lblEnterOrSelect = new Label(this, SWT.NONE);
+        lblEnterOrSelect.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
+        lblEnterOrSelect.setText("Enter or select the model:");
 
-	private void initCustomDataBindings() {
-		DataBindingContext bindingContext = new DataBindingContext();
-		
-		IObservableValue<String> observedDiagramName = WidgetProperties.text(SWT.Modify).observe(textDiagramName);
-		IObservableValue<String> observedModelName = WidgetProperties.text(SWT.Modify).observe(textModel);
-		
-		IObservableValue<String> atomicValidatedDiagramName = new WritableValue<String>(null, String.class);
-		IObservableValue<ModelName> atomicValidatedModelName = new WritableValue<ModelName>(null, ModelName.class);
-		
-		UpdateValueStrategy strategyAtomicModelNameTargetToModel = new UpdateValueStrategy();
-		strategyAtomicModelNameTargetToModel.setAfterGetValidator(new AtomicModelNameProcessor());
-		strategyAtomicModelNameTargetToModel.setConverter(new AtomicModelNameProcessor());
-		bindingContext.bindValue(observedModelName, atomicValidatedModelName, strategyAtomicModelNameTargetToModel, new UpdateValueStrategy(UpdateValueStrategy.POLICY_NEVER));
-		
-		UpdateValueStrategy strategyAtomicDiagramNameTargetToModel = new UpdateValueStrategy();
-		strategyAtomicDiagramNameTargetToModel.setAfterGetValidator(new AtomicDiagramNameProcessor());
-		bindingContext.bindValue(observedDiagramName, atomicValidatedDiagramName, strategyAtomicDiagramNameTargetToModel, new UpdateValueStrategy(UpdateValueStrategy.POLICY_NEVER));
-		
-		CompoundNameValidator diagramNameValidator = new CompoundNameValidator(atomicValidatedDiagramName, atomicValidatedModelName);
-		bindingContext.addValidationStatusProvider(diagramNameValidator);
-		IObservableValue<String> validatedDiagramName = diagramNameValidator.observeValidatedValue(atomicValidatedDiagramName);
-		IObservableValue<ModelName> validatedModelName = diagramNameValidator.observeValidatedValue(atomicValidatedModelName);
-		
-		IObservableValue<ModelName> modelSelectedModelName = BeanProperties.value(SelectedNamesDTO.class, "selectedModelName").observe(dto);
-		IObservableValue<String> modelSelectedDiagramName = BeanProperties.value(SelectedNamesDTO.class, "selectedDiagramName").observe(dto);
-		
-		// Bind to the validated value, not the middle/target
-		bindingContext.bindValue(validatedDiagramName, modelSelectedDiagramName);
-		bindingContext.bindValue(validatedModelName, modelSelectedModelName);
-		
-		aggregatedStatus = new AggregateValidationStatus(bindingContext, AggregateValidationStatus.MAX_SEVERITY);
-		aggregatedStatus.addChangeListener(validatorStatusListener);
-	}
-	
-	@Override
-	public void dispose() {
-		if (aggregatedStatus != null) {
-			aggregatedStatus.removeChangeListener(validatorStatusListener);
-		}
-		super.dispose();
-	}
-	
-	private static abstract class TextFieldVerifier implements VerifyListener {
-		
-		@Override
-		public void verifyText(VerifyEvent e) {
-			if (e.text == null) {
-				e.doit = true;
-			}
-			e.doit = allowModification(e.text);
-		}
+        textModel = new Text(this, SWT.BORDER);
+        textModel.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
 
-		protected abstract boolean allowModification(String text);
-		
-	}
-	
-	private void initCustomListeners() {
-		textDiagramName.addVerifyListener(new TextFieldVerifier() {
-			protected boolean allowModification(String text) {
-				return text.matches("[a-zA-Z0-9_ .]*");
-			}
-		});
-		textModel.addVerifyListener(new TextFieldVerifier() {
-			@Override
-			protected boolean allowModification(String text) {
-				return text.matches("[a-zA-Z0-9_ ./]*");
-			}});
-	}
-	
+        treeViewer = new TreeViewer(this, SWT.BORDER);
+        Tree tree = treeViewer.getTree();
+        tree.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+        treeViewer.setLabelProvider(new ModelLabelProvider());
+        treeViewer.setContentProvider(new ModelContentProvider());
+        treeViewer.setInput(dto);
+
+        Label lblDiagramName = new Label(this, SWT.NONE);
+        lblDiagramName.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+        lblDiagramName.setText("Diagram Name:");
+
+        textDiagramName = new Text(this, SWT.BORDER);
+        textDiagramName.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+        m_bindingContext = initDataBindings();
+        initCustomDataBindings();
+        initCustomListeners();
+    }
+
+    @Override
+    protected void checkSubclass() {
+        // Disable the check that prevents subclassing of SWT components
+    }
+
+    protected DataBindingContext initDataBindings() {
+        DataBindingContext bindingContext = new DataBindingContext();
+        //
+        IObservableValue<?> observeSingleSelectionTreeViewer = ViewerProperties.singleSelection().observe(treeViewer);
+        @SuppressWarnings("unchecked")
+        IObservableValue<?> textTextModelObserveValue = PojoProperties.value("text").observe(textModel);
+        UpdateValueStrategy strategy_1 = new UpdateValueStrategy();
+        strategy_1.setConverter(new ModelNodeConverter());
+        bindingContext.bindValue(observeSingleSelectionTreeViewer, textTextModelObserveValue, strategy_1,
+                new UpdateValueStrategy(UpdateValueStrategy.POLICY_NEVER));
+        //
+        return bindingContext;
+    }
+
+    private void initCustomDataBindings() {
+        DataBindingContext bindingContext = new DataBindingContext();
+
+        IObservableValue<?> observedDiagramName = WidgetProperties.text(SWT.Modify).observe(textDiagramName);
+        IObservableValue<?> observedModelName = WidgetProperties.text(SWT.Modify).observe(textModel);
+
+        IObservableValue<String> atomicValidatedDiagramName = new WritableValue<String>(null, String.class);
+        IObservableValue<ModelName> atomicValidatedModelName = new WritableValue<ModelName>(null, ModelName.class);
+
+        UpdateValueStrategy strategyAtomicModelNameTargetToModel = new UpdateValueStrategy();
+        strategyAtomicModelNameTargetToModel.setAfterGetValidator(new AtomicModelNameProcessor());
+        strategyAtomicModelNameTargetToModel.setConverter(new AtomicModelNameProcessor());
+        bindingContext.bindValue(observedModelName, atomicValidatedModelName, strategyAtomicModelNameTargetToModel,
+                new UpdateValueStrategy(UpdateValueStrategy.POLICY_NEVER));
+
+        UpdateValueStrategy strategyAtomicDiagramNameTargetToModel = new UpdateValueStrategy();
+        strategyAtomicDiagramNameTargetToModel.setAfterGetValidator(new AtomicDiagramNameProcessor());
+        bindingContext.bindValue(observedDiagramName, atomicValidatedDiagramName,
+                strategyAtomicDiagramNameTargetToModel, new UpdateValueStrategy(UpdateValueStrategy.POLICY_NEVER));
+
+        CompoundNameValidator diagramNameValidator = new CompoundNameValidator(atomicValidatedDiagramName,
+                atomicValidatedModelName);
+        bindingContext.addValidationStatusProvider(diagramNameValidator);
+        IObservableValue<?> validatedDiagramName = diagramNameValidator
+                .observeValidatedValue(atomicValidatedDiagramName);
+        IObservableValue<?> validatedModelName = diagramNameValidator.observeValidatedValue(atomicValidatedModelName);
+
+        @SuppressWarnings("unchecked")
+        IObservableValue<ModelName> modelSelectedModelName = BeanProperties
+                .value(SelectedNamesDTO.class, "selectedModelName").observe(dto);
+        @SuppressWarnings("unchecked")
+        IObservableValue<String> modelSelectedDiagramName = BeanProperties
+                .value(SelectedNamesDTO.class, "selectedDiagramName").observe(dto);
+
+        // Bind to the validated value, not the middle/target
+        bindingContext.bindValue(validatedDiagramName, modelSelectedDiagramName);
+        bindingContext.bindValue(validatedModelName, modelSelectedModelName);
+
+        aggregatedStatus = new AggregateValidationStatus(bindingContext, AggregateValidationStatus.MAX_SEVERITY);
+        aggregatedStatus.addChangeListener(validatorStatusListener);
+    }
+
+    @Override
+    public void dispose() {
+        if (aggregatedStatus != null) {
+            aggregatedStatus.removeChangeListener(validatorStatusListener);
+        }
+        super.dispose();
+    }
+
+    private static abstract class TextFieldVerifier implements VerifyListener {
+
+        @Override
+        public void verifyText(VerifyEvent e) {
+            if (e.text == null) {
+                e.doit = true;
+            }
+            e.doit = allowModification(e.text);
+        }
+
+        protected abstract boolean allowModification(String text);
+
+    }
+
+    private void initCustomListeners() {
+        textDiagramName.addVerifyListener(new TextFieldVerifier() {
+            protected boolean allowModification(String text) {
+                return text.matches("[a-zA-Z0-9_ .]*");
+            }
+        });
+        textModel.addVerifyListener(new TextFieldVerifier() {
+            @Override
+            protected boolean allowModification(String text) {
+                return text.matches("[a-zA-Z0-9_ ./]*");
+            }
+        });
+    }
+
 }
