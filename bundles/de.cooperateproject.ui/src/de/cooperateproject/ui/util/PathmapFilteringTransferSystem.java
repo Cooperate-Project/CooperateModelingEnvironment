651bc441af2aa9913b7ba9c1fb2bc152ac24351f
Stephan Seifermann <seifermann@fzi.de>
2017-03-15 14:02:20 +0100

    Fixed model gen checkout for derived state aware resources

--- bundles/de.cooperateproject.ui/src/de/cooperateproject/ui/util/PathmapFilteringTransferSystem.java
+++ bundles/de.cooperateproject.ui/src/de/cooperateproject/ui/util/PathmapFilteringTransferSystem.java
@@ -13,79 +13,87 @@ import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.xtext.resource.DerivedStateAwareResource;
 
 public class PathmapFilteringTransferSystem extends CDOTransferSystem {
 
-	public PathmapFilteringTransferSystem(CDOTransferSystem transferSystem) {
-		super(false);
-		this.transferSystem = transferSystem;
-	}
+    public PathmapFilteringTransferSystem(CDOTransferSystem transferSystem) {
+        super(false);
+        this.transferSystem = transferSystem;
+    }
 
-	private final CDOTransferSystem transferSystem;
+    private final CDOTransferSystem transferSystem;
 
-	public void saveModels(EList<Resource> resources, IProgressMonitor monitor) {
-		BasicEList<Resource> filteredResources = new BasicEList<>(
-				resources.stream().filter(r -> !r.getURI().scheme().equals("pathmap"))
-						.collect(Collectors.toList()));
-		transferSystem.saveModels(filteredResources, monitor);
-	}
+    public void saveModels(EList<Resource> resources, IProgressMonitor monitor) {
+        BasicEList<Resource> filteredResources = new BasicEList<>(
+                resources.stream().filter(r -> !r.getURI().scheme().equals("pathmap")).collect(Collectors.toList()));
+        resources.stream().filter(DerivedStateAwareResource.class::isInstance)
+                .map(DerivedStateAwareResource.class::cast).forEach(PathmapFilteringTransferSystem::calculateState);
+        ;
+        transferSystem.saveModels(filteredResources, monitor);
+    }
 
-	// delegation methods
-	
-	public int hashCode() {
-		return transferSystem.hashCode();
-	}
+    private static void calculateState(DerivedStateAwareResource resource) {
+        resource.discardDerivedState();
+        resource.installDerivedState(false);
+    }
 
-	public String getType() {
-		return transferSystem.getType();
-	}
+    // delegation methods
 
-	public boolean isReadOnly() {
-		return transferSystem.isReadOnly();
-	}
+    public int hashCode() {
+        return transferSystem.hashCode();
+    }
 
-	public CDOTransferType getDefaultTransferType(CDOTransferElement element) {
-		return transferSystem.getDefaultTransferType(element);
-	}
+    public String getType() {
+        return transferSystem.getType();
+    }
 
-	public URI getURI(IPath path) {
-		return transferSystem.getURI(path);
-	}
+    public boolean isReadOnly() {
+        return transferSystem.isReadOnly();
+    }
 
-	public CDOTransferElement getElement(String path) {
-		return transferSystem.getElement(path);
-	}
+    public CDOTransferType getDefaultTransferType(CDOTransferElement element) {
+        return transferSystem.getDefaultTransferType(element);
+    }
 
-	public CDOTransferElement getElement(IPath path) {
-		return transferSystem.getElement(path);
-	}
+    public URI getURI(IPath path) {
+        return transferSystem.getURI(path);
+    }
 
-	public CDOTransferElement getElement(URI uri) {
-		return transferSystem.getElement(uri);
-	}
+    public CDOTransferElement getElement(String path) {
+        return transferSystem.getElement(path);
+    }
 
-	public void createFolder(IPath path) {
-		transferSystem.createFolder(path);
-	}
+    public CDOTransferElement getElement(IPath path) {
+        return transferSystem.getElement(path);
+    }
 
-	public Resource createModel(ResourceSet resourceSet, IPath path) {
-		return transferSystem.createModel(resourceSet, path);
-	}
+    public CDOTransferElement getElement(URI uri) {
+        return transferSystem.getElement(uri);
+    }
 
-	public void createBinary(IPath path, InputStream source, IProgressMonitor monitor) {
-		transferSystem.createBinary(path, source, monitor);
-	}
+    public void createFolder(IPath path) {
+        transferSystem.createFolder(path);
+    }
 
-	public void createText(IPath path, InputStream source, String encoding, IProgressMonitor monitor) {
-		transferSystem.createText(path, source, encoding, monitor);
-	}
+    public Resource createModel(ResourceSet resourceSet, IPath path) {
+        return transferSystem.createModel(resourceSet, path);
+    }
 
-	public boolean equals(Object obj) {
-		return transferSystem.equals(obj);
-	}
+    public void createBinary(IPath path, InputStream source, IProgressMonitor monitor) {
+        transferSystem.createBinary(path, source, monitor);
+    }
 
-	public String toString() {
-		return transferSystem.toString();
-	}
+    public void createText(IPath path, InputStream source, String encoding, IProgressMonitor monitor) {
+        transferSystem.createText(path, source, encoding, monitor);
+    }
+
+    public boolean equals(Object obj) {
+        return transferSystem.equals(obj);
+    }
+
+    public String toString() {
+        return transferSystem.toString();
+    }
 
 }
