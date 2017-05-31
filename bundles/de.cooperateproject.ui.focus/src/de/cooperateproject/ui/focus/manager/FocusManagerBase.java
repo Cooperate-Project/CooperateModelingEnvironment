cdf7978c80417229b51ca773d1b488ac70ad6a75
thomcz <thomas.czogalik@gmx.de>
2017-05-31 11:25:07 +0200

    #76 Focus view has no dependencies to specific diagram types

--- bundles/de.cooperateproject.ui.focus/src/de/cooperateproject/ui/focus/manager/FocusManagerBase.java
+++ bundles/de.cooperateproject.ui.focus/src/de/cooperateproject/ui/focus/manager/FocusManagerBase.java
@@ -2,10 +2,24 @@ package de.cooperateproject.ui.focus.manager;
 
 import org.eclipse.ui.IEditorPart;
 
+/**
+ * Base class for FocusManager implementations.
+ * 
+ * @author Tischner, czogalik
+ *
+ * @param <T>
+ *            type of EditorPart
+ */
 public abstract class FocusManagerBase<T extends IEditorPart> implements IFocusManager {
 
     private final T editorPart;
 
+    /**
+     * Sets the actual editor part.
+     * 
+     * @param editorPart
+     *            the actual editor part.
+     */
     public FocusManagerBase(T editorPart) {
         this.editorPart = editorPart;
     }
