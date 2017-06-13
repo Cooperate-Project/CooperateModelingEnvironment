0ad2cb797dee76a489be7aa033d61c4055b11faf
thomcz <thomas.czogalik@gmx.de>
2017-06-13 10:58:40 +0200

    #97 Validator checks for unique named elements in cls editor

--- bundles/de.cooperateproject.modeling.textual.cls/src/de/cooperateproject/modeling/textual/cls/validation/ClsValidator.java
+++ bundles/de.cooperateproject.modeling.textual.cls/src/de/cooperateproject/modeling/textual/cls/validation/ClsValidator.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.uml2.uml.Element;
@@ -20,6 +21,8 @@ import de.cooperateproject.modeling.textual.cls.cls.ClsPackage;
 import de.cooperateproject.modeling.textual.cls.cls.Interface;
 import de.cooperateproject.modeling.textual.cls.cls.Package;
 import de.cooperateproject.modeling.textual.cls.cls.XtextAssociation;
+import de.cooperateproject.modeling.textual.common.metamodel.textualCommons.AliasedElement;
+import de.cooperateproject.modeling.textual.common.metamodel.textualCommons.NamedElement;
 import de.cooperateproject.modeling.textual.common.metamodel.textualCommons.PackageBase;
 import de.cooperateproject.modeling.textual.common.metamodel.textualCommons.TextualCommonsPackage;
 import de.cooperateproject.modeling.textual.xtext.runtime.issues.IIssueCodeRegistry;
@@ -33,6 +36,7 @@ public class ClsValidator extends AbstractClsValidator {
     private static final String NOT_AN_INTERFACE = "not_an_interface";
     private static final String NOT_A_CLASS = "not_a_class";
     private static final String ALIAS_TAKEN = "alias_taken";
+    private static final String NAME_TAKEN = "name_taken";
     private static final String NOT_ENOUGH_ROLE_NAMES = "not_enough_role_names";
 
     @Inject
@@ -52,25 +56,57 @@ public class ClsValidator extends AbstractClsValidator {
     }
 
     @Check
-    private void checkUniqueAliasInItsPackage(Classifier<? extends org.eclipse.uml2.uml.Classifier> classifier) {
-        PackageBase<?> nearestPackage = classifier.getNearestPackage();
+    private void checkUniqueNamedElementInItsPackage(NamedElement element) {
+        PackageBase<?> nearestPackage = element.getNearestPackage();
         if (!(nearestPackage instanceof Package)) {
             return;
         }
-        List<Classifier<? extends org.eclipse.uml2.uml.Classifier>> classifiers = ((Package) nearestPackage)
-                .getClassifiers();
 
-        checkUniqueAlias(classifier, classifiers);
+        checkUniqueElements(element, getNamedElementsFromPackage((Package) nearestPackage));
     }
 
-    private void checkUniqueAlias(Classifier<? extends org.eclipse.uml2.uml.Classifier> classifier,
-            List<Classifier<? extends org.eclipse.uml2.uml.Classifier>> classifiers) {
-        for (Classifier<? extends org.eclipse.uml2.uml.Classifier> cls : classifiers) {
-            if (cls.equals(classifier)) {
+    private static List<? extends NamedElement> getNamedElementsFromPackage(Package nearestPackage) {
+
+        return Stream
+                .concat(nearestPackage.getClassifiers().stream(),
+                        nearestPackage.getConnectors().stream().filter(x -> x instanceof NamedElement)
+                                .map(x -> (NamedElement) x).collect(Collectors.toList()).stream())
+                .collect(Collectors.toList());
+    }
+
+    private void checkUniqueElements(NamedElement element, List<? extends NamedElement> namedElements) {
+
+        for (NamedElement namedElement : namedElements) {
+            if (element.equals(namedElement)) {
                 continue;
             }
-            if (cls.getAlias().equals(classifier.getAlias())) {
-                error("\"" + classifier.getAlias() + "\"" + " Alias is taken!",
+            if (element.getName().equals(namedElement.getName())) {
+                error("\"" + element.getName() + "\"" + " no duplicates!",
+                        TextualCommonsPackage.Literals.NAMED_ELEMENT__NAME, NAME_TAKEN);
+            }
+        }
+
+    }
+
+    @Check
+    private void checkUniqueAliasInItsPackage(AliasedElement aliasedElement) {
+        PackageBase<?> nearestPackage = aliasedElement.getNearestPackage();
+        if (!(nearestPackage instanceof Package)) {
+            return;
+        }
+        List<? extends AliasedElement> classifiers = ((Package) nearestPackage).getClassifiers();
+
+        checkUniqueAlias(aliasedElement, classifiers);
+    }
+
+    private void checkUniqueAlias(AliasedElement aliasedElement, List<? extends AliasedElement> aliasedElements) {
+        for (AliasedElement alias : aliasedElements) {
+            if (alias.equals(aliasedElement)) {
+                continue;
+            }
+            String classifierAlias = aliasedElement.getAlias();
+            if (alias.getAlias().equals(classifierAlias)) {
+                error("\"" + classifierAlias + "\"" + " Alias is taken!",
                         TextualCommonsPackage.Literals.ALIASED_ELEMENT__ALIAS, ALIAS_TAKEN);
             }
         }
