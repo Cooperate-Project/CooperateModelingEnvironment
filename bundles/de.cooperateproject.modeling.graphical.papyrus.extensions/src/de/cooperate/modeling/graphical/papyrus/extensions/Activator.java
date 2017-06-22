7cbb7da26f889a1258f502e276722fed1b23a00d
Philipp Faller <faller@fzi.de>
2017-06-22 14:40:16 +0200

    #6 Reduced Property Views

--- bundles/de.cooperateproject.modeling.graphical.papyrus.extensions/src/de/cooperate/modeling/graphical/papyrus/extensions/Activator.java
+++ bundles/de.cooperateproject.modeling.graphical.papyrus.extensions/src/de/cooperate/modeling/graphical/papyrus/extensions/Activator.java
@@ -1,10 +1,14 @@
 package de.cooperate.modeling.graphical.papyrus.extensions;
 
+import java.util.Collection;
+
+import org.eclipse.papyrus.infra.properties.contexts.Context;
 import org.eclipse.papyrus.infra.viewpoints.configuration.PapyrusConfiguration;
 import org.eclipse.papyrus.infra.viewpoints.configuration.PapyrusViewpoint;
 import org.eclipse.papyrus.infra.viewpoints.iso42010.Stakeholder;
 import org.eclipse.papyrus.infra.viewpoints.policy.PolicyChecker;
 import org.eclipse.papyrus.infra.viewpoints.policy.WeightedConfiguration;
+import org.eclipse.papyrus.views.properties.runtime.ConfigurationManager;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -12,6 +16,7 @@ public class Activator extends AbstractUIPlugin {
 
     @Override
     public void start(BundleContext context) throws Exception {
+        // TODO Setting default viewpoint unnecessary?
         super.start(context);
         WeightedConfiguration weightedConfig = WeightedConfiguration.getTopConfiguration();
         if (null != weightedConfig) {
@@ -25,6 +30,18 @@ public class Activator extends AbstractUIPlugin {
             PolicyChecker newPolicy = new PolicyChecker(config, viewpoint, false);
             PolicyChecker.setCurrent(newPolicy);
         }
+        enableCooperatePropertyContext();
+    }
+
+    public static void enableCooperatePropertyContext() {
+        Collection<Context> contexts = ConfigurationManager.getInstance().getContexts();
+        contexts.forEach(ctx -> {
+            if (!"CooperateProperties".equals(ctx.getName())) {
+                ConfigurationManager.getInstance().disableContext(ctx, true);
+            } else {
+                ConfigurationManager.getInstance().enableContext(ctx, true);
+            }
+        });
     }
 
 }
