a20b0c3c6f7120ac05837a6fe1ddd3afc596256f
joehen <joerghe@gmail.com>
2017-07-11 15:53:37 +0200

    Use Activator to customize existing UML DiagramKinds

--- bundles/de.cooperateproject.modeling.graphical.papyrus.extensions/src/de/cooperate/modeling/graphical/papyrus/extensions/Activator.java
+++ bundles/de.cooperateproject.modeling.graphical.papyrus.extensions/src/de/cooperate/modeling/graphical/papyrus/extensions/Activator.java
@@ -3,6 +3,7 @@ package de.cooperate.modeling.graphical.papyrus.extensions;
 import java.util.Collection;
 
 import org.eclipse.core.runtime.Plugin;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.papyrus.infra.properties.contexts.Context;
 import org.eclipse.papyrus.views.properties.runtime.ConfigurationManager;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -13,6 +14,7 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends AbstractUIPlugin {
 
+    private static final String COOPERATE_DIAGRAMS_ARCHITECTURE = "resource/CooperateDiagrams.architecture";
     private static Plugin instance;
 
     public static Plugin getPluginInstance() {
@@ -23,6 +25,7 @@ public class Activator extends AbstractUIPlugin {
     public void start(BundleContext context) throws Exception {
         super.start(context);
         setInstance(this);
+        registerCooperateViewpointExtension();
         enableCooperatePropertyContext();
     }
 
@@ -34,6 +37,14 @@ public class Activator extends AbstractUIPlugin {
 
     private static void setInstance(Plugin instance) {
         Activator.instance = instance;
+    }
+
+    private static void registerCooperateViewpointExtension() {
+        String bundleName = Activator.getPluginInstance().getBundle().getSymbolicName();
+        String pathName = String.format("/%s/%s", bundleName, COOPERATE_DIAGRAMS_ARCHITECTURE);
+        URI architectureURI = URI.createPlatformPluginURI(pathName, true);
+        PapyrusViewpointCustomizationHelper.mergeDiagramKinds(architectureURI);
+
     }
 
     public static void enableCooperatePropertyContext() {
