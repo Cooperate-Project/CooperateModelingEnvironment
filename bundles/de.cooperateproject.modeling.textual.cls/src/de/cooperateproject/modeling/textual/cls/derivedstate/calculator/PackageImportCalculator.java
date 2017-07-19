7ab3b636aa1816c1009db4b7690abac5b7bfdb77
joehen <joergh@gmail.com>
2017-07-19 11:32:41 +0200

    #122 Use a dedicated calculator for package imports

---
+++ bundles/de.cooperateproject.modeling.textual.cls/src/de/cooperateproject/modeling/textual/cls/derivedstate/calculator/PackageImportCalculator.java
@@ -0,0 +1,44 @@
+package de.cooperateproject.modeling.textual.cls.derivedstate.calculator;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.eclipse.emf.ecore.EObject;
+
+import de.cooperateproject.modeling.textual.common.metamodel.textualCommons.PackageImport;
+import de.cooperateproject.modeling.textual.common.metamodel.textualCommons.UMLReferencingElement;
+import de.cooperateproject.modeling.textual.xtext.runtime.derivedstate.initializer.Applicability;
+import de.cooperateproject.modeling.textual.xtext.runtime.derivedstate.initializer.AtomicDerivedStateProcessorBase;
+import de.cooperateproject.modeling.textual.xtext.runtime.derivedstate.initializer.DerivedStateProcessorApplicability;
+
+/**
+ * State calculator for package imports.
+ */
+@Applicability(applicabilities = DerivedStateProcessorApplicability.CALCULATION)
+public class PackageImportCalculator extends AtomicDerivedStateProcessorBase<PackageImport> {
+
+    /**
+     * Constructs the calculator.
+     */
+    public PackageImportCalculator() {
+        super(PackageImport.class);
+    }
+
+    @Override
+    protected void applyTyped(PackageImport object) {
+
+        if (object.getImportingPackage() != null && object.getImportingPackage().getReferencedElement() != null
+                && object.getImportedPackage() != null) {
+            org.eclipse.uml2.uml.PackageImport umlPackageImport = object.getImportingPackage().getReferencedElement()
+                    .getPackageImport(object.getImportedPackage());
+            object.setReferencedElement(umlPackageImport);
+            return;
+        }
+    }
+
+    @Override
+    public Collection<Class<? extends EObject>> getReplacements() {
+        return Arrays.asList(UMLReferencingElement.class);
+    }
+
+}
