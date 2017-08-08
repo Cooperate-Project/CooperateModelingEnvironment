9396ff6cb37a777c2b610347a6c6c1c1d6422d6d
Stephan Seifermann <seifermann@fzi.de>
2017-08-08 10:35:17 +0200

    #129 Support project names including spaces
    * Use patched cdo-xtext version that does not enforce URI encoding
    * Encode URI of UML model based on the encoding status of the input URI
    * Do not enforce encoding of URI when loading launcher model

--- bundles/de.cooperateproject.modeling.textual.xtext.runtime/src/de/cooperateproject/modeling/textual/xtext/runtime/scoping/ConventionalUMLUriFinder.java
+++ bundles/de.cooperateproject.modeling.textual.xtext.runtime/src/de/cooperateproject/modeling/textual/xtext/runtime/scoping/ConventionalUMLUriFinder.java
@@ -8,6 +8,11 @@ import org.slf4j.LoggerFactory;
 
 import de.cooperateproject.modeling.common.conventions.ModelNamingConventions;
 
+/**
+ * Constructs the UML model name based on the URI of the textual modeling resource.
+ * 
+ * @see ModelNamingConventions#getUMLFromTextualURI(URI)
+ */
 public class ConventionalUMLUriFinder implements IUMLUriFinder {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ConventionalUMLUriFinder.class);
@@ -15,11 +20,24 @@ public class ConventionalUMLUriFinder implements IUMLUriFinder {
     @Override
     public Optional<URI> findUMLURI(URI ownUri) {
         try {
-            return Optional.of(ModelNamingConventions.getUMLFromTextualURI(ownUri));
+            return Optional.of(ModelNamingConventions.getUMLFromTextualURI(ownUri)).map(u -> normalize(ownUri, u));
         } catch (IllegalArgumentException e) {
             LOGGER.warn("Could not determine the UML URI on a regular basis.", e);
             return Optional.empty();
         }
     }
 
+    private static URI normalize(URI ownUri, URI toNormalize) {
+        if (isEncoded(ownUri)) {
+            return URI.createURI(URI.decode(toNormalize.toString()), false);
+        } else {
+            return URI.createURI(URI.decode(toNormalize.toString()));
+        }
+    }
+
+    private static boolean isEncoded(URI uri) {
+        String originalUri = uri.toString();
+        String decodedUri = URI.decode(originalUri);
+        return !originalUri.equals(decodedUri);
+    }
 }
