2a7ac0673b0719212f782aad61ce5dfe0c592a8b
Stephan Seifermann <seifermann@fzi.de>
2017-08-18 17:13:28 +0200

    Added mechanism to debug results of model transformations.
    Call setDebugSerializationDir at the beginning of your test to serialize
    the results into a directory.

--- tests/de.cooperateproject.modeling.transformation.transformations.tests/src/de/cooperateproject/modeling/transformation/transformations/tests/transforms/plain/PlainTransformationTestBase.java
+++ tests/de.cooperateproject.modeling.transformation.transformations.tests/src/de/cooperateproject/modeling/transformation/transformations/tests/transforms/plain/PlainTransformationTestBase.java
@@ -5,15 +5,20 @@ import static org.junit.Assert.assertTrue;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.emf.common.util.URI;
@@ -26,9 +31,11 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.eclipse.m2m.qvt.oml.BasicModelExtent;
 import org.eclipse.m2m.qvt.oml.ModelExtent;
 import org.eclipse.m2m.qvt.oml.util.Trace;
+import org.junit.After;
 
 import de.cooperateproject.modeling.transformation.transformations.tests.util.ModelComparator;
 
@@ -36,6 +43,16 @@ public abstract class PlainTransformationTestBase extends TransformationTestBase
 
     private static final URI UML_PRIMITIVE_TYPES = URI
             .createURI("pathmap://UML_LIBRARIES/EcorePrimitiveTypes.library.uml");
+    private File debugSerializationDir = null;
+
+    @After
+    public void tearDown() {
+        setDebugSerializationDir(null);
+    }
+
+    protected void setDebugSerializationDir(File dir) {
+        debugSerializationDir = dir;
+    }
 
     protected ModelExtent runTransformation(URI transformationURI, URI sourceModelURI, URI umlModelURI)
             throws IOException {
@@ -176,5 +193,65 @@ public abstract class PlainTransformationTestBase extends TransformationTestBase
     protected ModelExtent createModelExtent(URI resourceURI) throws IOException {
         Resource r = createResource(getResourceSet(), resourceURI);
         return createModelExtent(r);
+    }
+
+    protected void debugSerialize(EObject expected, EObject actual) throws IOException {
+        debugSerialize(Arrays.asList(expected), Arrays.asList(actual));
+    }
+
+    @SuppressWarnings("squid:S2583")
+    protected void debugSerialize(Collection<EObject> expected, Collection<EObject> actual) throws IOException {
+        if (debugSerializationDir == null) {
+            return;
+        }
+
+        URI expectedURI = findAnyURI(expected)
+                .orElse(URI.createFileURI(new File(debugSerializationDir, "expected.xmi").getAbsolutePath()));
+        serialize(expected, expectedURI, "expected");
+
+        URI actualURI = findAnyURI(actual).orElse(
+                expectedURI.trimSegments(1).appendSegment(expectedURI.trimFileExtension().lastSegment() + "-actual")
+                        .appendFileExtension(expectedURI.fileExtension()));
+        serialize(actual, actualURI, "actual");
+    }
+
+    private static Optional<URI> findAnyURI(Collection<EObject> elements) {
+        return elements.stream().map(EObject::eResource).filter(Objects::nonNull).map(Resource::getURI).findAny();
+    }
+
+    private void serialize(Collection<EObject> model, URI uri, String fileName) throws IOException {
+        assertTrue(model.stream().map(EObject::eResource).distinct().count() == 1);
+        Resource r = Optional.ofNullable(model.iterator().next().eResource())
+                .orElseGet(() -> createAndAssignToResource(model, uri));
+        FileUtils.write(new File(debugSerializationDir, String.format("%s.%s", fileName, uri.fileExtension())),
+                serialize(r, true));
+    }
+
+    private Resource createAndAssignToResource(Collection<EObject> model, URI uri) {
+        Resource r = getResourceSet().createResource(uri);
+        r.getContents().addAll(model);
+        return r;
+    }
+
+    private static String serialize(Resource r, boolean withoutXmiIds) throws IOException {
+        @SuppressWarnings("deprecation")
+        Optional<Map<EObject, String>> idMap = Optional.of(r).filter(XMLResource.class::isInstance)
+                .map(XMLResource.class::cast).map(XMLResource::getEObjectToIDMap);
+        Map<EObject, String> oldIds = new HashMap<>(idMap.orElse(Collections.emptyMap()));
+        try {
+            if (withoutXmiIds) {
+                idMap.ifPresent(Map::clear);
+            }
+            return serialize(r);
+        } finally {
+            idMap.ifPresent(m -> m.putAll(oldIds));
+        }
+    }
+
+    private static String serialize(Resource r) throws IOException {
+        try (OutputStream os = new ByteArrayOutputStream()) {
+            r.save(os, Collections.emptyMap());
+            return os.toString();
+        }
     }
 }
