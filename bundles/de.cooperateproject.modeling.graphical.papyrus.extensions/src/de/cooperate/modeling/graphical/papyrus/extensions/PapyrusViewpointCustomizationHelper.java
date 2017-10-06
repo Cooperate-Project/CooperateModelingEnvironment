27a6b328303742323683cef004cf5b30593f8c42
joehen <joergh@gmail.com>
2017-10-06 14:25:18 +0200

    Patch for use case subject insertion

--- bundles/de.cooperateproject.modeling.graphical.papyrus.extensions/src/de/cooperate/modeling/graphical/papyrus/extensions/PapyrusViewpointCustomizationHelper.java
+++ bundles/de.cooperateproject.modeling.graphical.papyrus.extensions/src/de/cooperate/modeling/graphical/papyrus/extensions/PapyrusViewpointCustomizationHelper.java
@@ -2,8 +2,10 @@ package de.cooperate.modeling.graphical.papyrus.extensions;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -12,7 +14,13 @@ import org.eclipse.papyrus.infra.architecture.ArchitectureDomainManager;
 import org.eclipse.papyrus.infra.core.architecture.ArchitectureDescriptionLanguage;
 import org.eclipse.papyrus.infra.core.architecture.ArchitectureDomain;
 import org.eclipse.papyrus.infra.core.architecture.RepresentationKind;
+import org.eclipse.papyrus.infra.gmfdiag.paletteconfiguration.DrawerConfiguration;
+import org.eclipse.papyrus.infra.gmfdiag.paletteconfiguration.ElementDescriptor;
+import org.eclipse.papyrus.infra.gmfdiag.paletteconfiguration.PaletteConfiguration;
+import org.eclipse.papyrus.infra.gmfdiag.paletteconfiguration.ToolConfiguration;
 import org.eclipse.papyrus.infra.gmfdiag.representation.PapyrusDiagram;
+import org.eclipse.papyrus.infra.types.ElementTypeConfiguration;
+import org.eclipse.papyrus.infra.types.SpecializationTypeConfiguration;
 
 public class PapyrusViewpointCustomizationHelper {
 
@@ -39,6 +47,30 @@ public class PapyrusViewpointCustomizationHelper {
             targetDiagram.getChildRules().addAll(sourceDiagram.getChildRules());
             targetDiagram.getModelRules().addAll(sourceDiagram.getModelRules());
             targetDiagram.getOwningRules().addAll(sourceDiagram.getOwningRules());
+
+            Optional<ToolConfiguration> toolConfig = targetDiagram.getPalettes().stream()
+                    .filter(p -> p.getId().equals("org.eclipse.papyrus.uml.diagram.usecase.paletteconfiguration"))
+                    .map(PaletteConfiguration::getDrawerConfigurations).flatMap(l -> l.stream())
+                    .filter(dc -> dc.getId().equals("usecase.group.nodes"))
+                    .map(DrawerConfiguration::getOwnedConfigurations).flatMap(l -> l.stream())
+                    .filter(config -> config.getId().equals("usecase.tool.subject"))
+                    .filter(ToolConfiguration.class::isInstance).map(ToolConfiguration.class::cast).findAny();
+
+            // Patch the available set of classifiers
+            if (toolConfig.isPresent()) {
+                ToolConfiguration t = toolConfig.get();
+                ElementDescriptor e0 = t.getElementDescriptors().get(0);
+                ElementDescriptor e1 = t.getElementDescriptors().get(1);
+
+                EList<ElementTypeConfiguration> types = ((SpecializationTypeConfiguration) e0.getElementType())
+                        .getSpecializedTypes();
+                EList<ElementTypeConfiguration> componentTypes = ((SpecializationTypeConfiguration) e1.getElementType())
+                        .getSpecializedTypes();
+                types.clear();
+                types.addAll(componentTypes);
+
+            }
+
         }
 
     }
