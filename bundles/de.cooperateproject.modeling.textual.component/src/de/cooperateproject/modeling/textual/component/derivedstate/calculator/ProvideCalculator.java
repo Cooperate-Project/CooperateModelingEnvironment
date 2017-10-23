030743a00286496ace3ee1504c78ddc0ca5a7712
joehen <joergh@gmail.com>
2017-10-23 18:33:31 +0200

    Provide calculators

--- bundles/de.cooperateproject.modeling.textual.component/src/de/cooperateproject/modeling/textual/component/derivedstate/calculator/ProvideCalculator.java
+++ bundles/de.cooperateproject.modeling.textual.component/src/de/cooperateproject/modeling/textual/component/derivedstate/calculator/ProvideCalculator.java
@@ -8,42 +8,44 @@ import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.uml2.uml.DirectedRelationship;
 import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.InterfaceRealization;
 import org.eclipse.uml2.uml.UMLFactory;
 import org.eclipse.uml2.uml.UMLPackage;
 import org.eclipse.uml2.uml.Usage;
 
 import de.cooperateproject.modeling.textual.common.metamodel.textualCommons.UMLReferencingElement;
 import de.cooperateproject.modeling.textual.component.cmp.Classifier;
+import de.cooperateproject.modeling.textual.component.cmp.Provide;
 import de.cooperateproject.modeling.textual.component.cmp.Require;
 import de.cooperateproject.modeling.textual.xtext.runtime.derivedstate.initializer.Applicability;
 import de.cooperateproject.modeling.textual.xtext.runtime.derivedstate.initializer.AtomicDerivedStateProcessorBase;
 import de.cooperateproject.modeling.textual.xtext.runtime.derivedstate.initializer.DerivedStateProcessorApplicability;
 
 /**
- * State calculator for Require.
+ * State calculator for Provide.
  */
 @Applicability(applicabilities = DerivedStateProcessorApplicability.CALCULATION)
-public class RequireCalculator extends AtomicDerivedStateProcessorBase<Require> {
+public class ProvideCalculator extends AtomicDerivedStateProcessorBase<Provide> {
 
-	public RequireCalculator() {
-		super(Require.class);
+	public ProvideCalculator() {
+		super(Provide.class);
 	}
 
 	@Override
-	protected void applyTyped(Require object) {
+	protected void applyTyped(Provide object) {
 		if (object.eContainer() instanceof Classifier) {
 			Classifier classifier = (Classifier) object.eContainer();
 			if (classifier.getReferencedElement() != null && object.getInterface() != null
 					&& object.getInterface().getReferencedElement() != null) {
 				Element umlClassifier = classifier.getReferencedElement();
-				EList<DirectedRelationship> usages = umlClassifier
-						.getSourceDirectedRelationships(UMLPackage.eINSTANCE.getUsage());
-				Optional<Usage> umlUsage = usages.stream().map(Usage.class::cast)
-						.filter(u -> u.getSuppliers().contains(object.getInterface().getReferencedElement()))
+				EList<DirectedRelationship> realizations = umlClassifier
+						.getSourceDirectedRelationships(UMLPackage.eINSTANCE.getInterfaceRealization());
+				Optional<InterfaceRealization> umlRealization = realizations.stream().map(InterfaceRealization.class::cast)
+						.filter(u -> u.getContract() == object.getInterface().getReferencedElement())
 						.filter(u -> object.getName() == null? true : u.getName().equals(object.getName()))
 						.findFirst();
-				if (umlUsage.isPresent()) {
-					object.setReferencedElement(umlUsage.get());
+				if (umlRealization.isPresent()) {
+					object.setReferencedElement(umlRealization.get());
 				}
 			}
 		}
