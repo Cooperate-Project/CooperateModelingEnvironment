58695d2c67806f9ee4885bf9fc61c95a1d731a08
joehen <joergh@gmail.com>
2017-10-25 13:46:51 +0200

    ConnectorEnd calculators

---
+++ bundles/de.cooperateproject.modeling.textual.component/src/de/cooperateproject/modeling/textual/component/derivedstate/initializer/ConnectorEndInitializer.java
@@ -0,0 +1,57 @@
+package de.cooperateproject.modeling.textual.component.derivedstate.initializer;
+
+import static de.cooperateproject.modeling.textual.xtext.runtime.derivedstate.initializer.DerivedStateProcessorApplicability.INITIALIZATION;
+
+import java.util.Optional;
+
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.uml2.uml.ConnectableElement;
+import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.Property;
+import org.eclipse.uml2.uml.Type;
+
+import de.cooperateproject.modeling.textual.common.util.UMLReferencingElementFinder;
+import de.cooperateproject.modeling.textual.component.cmp.Attribute;
+import de.cooperateproject.modeling.textual.component.cmp.Classifier;
+import de.cooperateproject.modeling.textual.component.cmp.ConnectorEnd;
+import de.cooperateproject.modeling.textual.component.cmp.Interface;
+import de.cooperateproject.modeling.textual.component.cmp.Port;
+import de.cooperateproject.modeling.textual.xtext.runtime.derivedstate.initializer.Applicability;
+import de.cooperateproject.modeling.textual.xtext.runtime.derivedstate.initializer.AtomicDerivedStateProcessorBase;
+
+/**
+ * Initializes the state of {@link Attribute} elements.
+ */
+@Applicability(applicabilities = INITIALIZATION)
+public class ConnectorEndInitializer extends AtomicDerivedStateProcessorBase<ConnectorEnd> {
+
+	public ConnectorEndInitializer() {
+		super(ConnectorEnd.class);
+	}
+
+	@Override
+	protected void applyTyped(ConnectorEnd object) {
+		if (object.getReferencedElement() != null) {
+			org.eclipse.uml2.uml.ConnectorEnd umlEnd = object.getReferencedElement();
+			Property umlPart = umlEnd.getPartWithPort();
+			ConnectableElement umlRole = umlEnd.getRole();
+
+			UMLReferencingElementFinder finder = UMLReferencingElementFinder.create(EcoreUtil.getRootContainer(object));
+
+			if (umlPart != null) {
+				Optional<Attribute> attribute = finder.findElement(umlPart, Attribute.class);
+				if (attribute.isPresent())
+					object.setPart(attribute.get());
+			}
+
+			if (umlRole != null) {
+				Optional<Port> port = finder.findElement(umlRole, Port.class);
+				if (port.isPresent())
+					object.setRole(port.get());
+			}
+
+		}
+
+	}
+
+}
