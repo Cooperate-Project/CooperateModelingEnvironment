f47428b342f542d9274e0f2b3d31bd9bd645207c
joehen <joergh@gmail.com>
2017-10-25 17:35:59 +0200

    resolve Classifiers before other things

--- bundles/de.cooperateproject.modeling.textual.component/src/de/cooperateproject/modeling/textual/component/services/ComponentDerivedStateComputerSorter.java
+++ bundles/de.cooperateproject.modeling.textual.component/src/de/cooperateproject/modeling/textual/component/services/ComponentDerivedStateComputerSorter.java
@@ -2,13 +2,16 @@ package de.cooperateproject.modeling.textual.component.services;
 
 import org.eclipse.emf.ecore.EObject;
 
+import de.cooperateproject.modeling.textual.component.cmp.Classifier;
 import de.cooperateproject.modeling.textual.xtext.runtime.derivedstate.initializer.IDerivedStateComputerSorter;
 
 public class ComponentDerivedStateComputerSorter implements IDerivedStateComputerSorter {
 
 	@Override
 	public int compare(EObject o1, EObject o2) {
-		return 0;
+		int prio1 = o1 instanceof Classifier ? 1 : 0;
+        int prio2 = o2 instanceof Classifier ? 1 : 0;
+        return prio2 - prio1;
 	}
 
 }
