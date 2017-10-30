023420d83aa7e5a6bf1b1b58c317290020cce5f7
joehen <joergh@gmail.com>
2017-10-30 11:51:47 +0100

    #208 added command to set and reset the style for actors

---
+++ bundles/de.cooperateproject.modeling.graphical.papyrus.extensions/src/de/cooperate/modeling/graphical/papyrus/extensions/usecase/ChangeActorStyleCommand.java
@@ -0,0 +1,44 @@
+package de.cooperate.modeling.graphical.papyrus.extensions.usecase;
+
+import java.util.Collections;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
+import org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand;
+import org.eclipse.gmf.runtime.notation.View;
+
+/**
+ * Change the css style of an actor to the given ActorKind
+ * 
+ * @author henss
+ *
+ */
+public class ChangeActorStyleCommand extends AbstractTransactionalCommand {
+    ActorKind changeTo;
+    protected GraphicalEditPart elementEditPart;
+
+    public ChangeActorStyleCommand(ActorKind changeTo, TransactionalEditingDomain domain, GraphicalEditPart classView) {
+        super(domain, "ChangeActorStyleCommand", Collections.EMPTY_LIST);
+        this.elementEditPart = classView;
+        this.changeTo = changeTo;
+    }
+
+    @Override
+    protected CommandResult doExecuteWithResult(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+        View actorview = elementEditPart.getNotationView();
+        switch (changeTo) {
+        case MACHINE:
+            ActorStyles.setActorMachine(actorview);
+            break;
+        default:
+            ActorStyles.setActorSticky(actorview);
+        }
+
+        return null;
+    }
+
+}
---
+++ bundles/de.cooperateproject.modeling.graphical.papyrus.extensions/src/de/cooperate/modeling/graphical/papyrus/extensions/usecase/ChangetoStickyShapeHandler.java
@@ -0,0 +1,34 @@
+package de.cooperate.modeling.graphical.papyrus.extensions.usecase;
+
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
+import org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand;
+import org.eclipse.papyrus.uml.diagram.usecase.edit.parts.ActorEditPartTN;
+import org.eclipse.uml2.uml.Actor;
+
+import de.cooperate.modeling.graphical.papyrus.extensions.ChangeStyleHandler;
+
+public class ChangetoStickyShapeHandler extends ChangeStyleHandler implements IHandler {
+
+    @Override
+    public boolean isEnabled() {
+        GraphicalEditPart editPart = getSelectedGraphicalEditpart();
+        if (editPart == null) {
+            return false;
+        }
+
+        if (((editPart.resolveSemanticElement() instanceof Actor) && (editPart instanceof ActorEditPartTN)
+                && ActorStyles.getActorKind(editPart.getNotationView()) == ActorKind.MACHINE)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected AbstractTransactionalCommand getChangeStyleCommand(GraphicalEditPart editPart) {
+        ChangeActorStyleCommand command = new ChangeActorStyleCommand(ActorKind.STICKY, editPart.getEditingDomain(),
+                editPart);
+        return command;
+    }
+
+}
