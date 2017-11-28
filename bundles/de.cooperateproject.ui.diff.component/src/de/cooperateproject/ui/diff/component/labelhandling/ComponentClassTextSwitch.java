2409cecfa78b1b8d75c6f9b0cba427dfa85e8112
thomcz <thomas.czogalik@gmx.de>
2017-11-28 15:26:43 +0100

    #243 added more functionality to component diff view

--- bundles/de.cooperateproject.ui.diff.component/src/de/cooperateproject/ui/diff/component/labelhandling/ComponentClassTextSwitch.java
+++ bundles/de.cooperateproject.ui.diff.component/src/de/cooperateproject/ui/diff/component/labelhandling/ComponentClassTextSwitch.java
@@ -1,7 +1,21 @@
 package de.cooperateproject.ui.diff.component.labelhandling;
 
+
+import de.cooperateproject.modeling.textual.component.cmp.Abstraction;
+import de.cooperateproject.modeling.textual.component.cmp.Attribute;
 import de.cooperateproject.modeling.textual.component.cmp.Component;
+import de.cooperateproject.modeling.textual.component.cmp.Connector;
+import de.cooperateproject.modeling.textual.component.cmp.Dependency;
+import de.cooperateproject.modeling.textual.component.cmp.Generalization;
+import de.cooperateproject.modeling.textual.component.cmp.Manifestation;
+import de.cooperateproject.modeling.textual.component.cmp.Method;
+import de.cooperateproject.modeling.textual.component.cmp.Port;
+import de.cooperateproject.modeling.textual.component.cmp.Provide;
+import de.cooperateproject.modeling.textual.component.cmp.Require;
 import de.cooperateproject.modeling.textual.component.cmp.RootPackage;
+import de.cooperateproject.modeling.textual.component.cmp.Substitution;
+import de.cooperateproject.modeling.textual.component.cmp.Class;
+import de.cooperateproject.modeling.textual.component.cmp.Interface;
 import de.cooperateproject.modeling.textual.component.cmp.util.CmpSwitch;
 
 public class ComponentClassTextSwitch extends CmpSwitch<String>{
@@ -15,4 +29,58 @@ public class ComponentClassTextSwitch extends CmpSwitch<String>{
     public String caseComponent(Component object) {
         return "component";
     }
+    @Override
+    public String caseClass(Class object) {
+        return "class";
+    }
+    @Override
+    public String caseInterface(Interface object) {
+        return "interface";
+    }
+    @Override
+    public String caseGeneralization(Generalization object) {
+        return "isa";
+    }
+    @Override
+    public String caseAbstraction(Abstraction object) {
+        return "abs";
+    }
+    @Override
+    public String caseManifestation(Manifestation object) {
+        return "man";
+    }
+    @Override
+    public String caseSubstitution(Substitution object) {
+        return "sub";
+    }
+    @Override
+    public String caseDependency(Dependency object) {
+        return "dep";
+    }
+    @Override
+    public String caseRequire(Require object) {
+        return "require";
+    }
+    @Override
+    public String caseProvide(Provide object) {
+        return "provide";
+    }
+    @Override
+    public String caseConnector(Connector object) {
+        return "connector";
+    }
+    @Override
+    public String casePort(Port object) {
+        return "port";
+    }
+    @Override
+    public String caseAttribute(Attribute object) {
+        return "attribute";
+    }
+    @Override
+    public String caseMethod(Method object) {
+        return "method";
+    }
+    
+    
 }
--- bundles/de.cooperateproject.ui.diff.component/src/de/cooperateproject/ui/diff/component/labelhandling/ComponentLabelSwitch.java
+++ bundles/de.cooperateproject.ui.diff.component/src/de/cooperateproject/ui/diff/component/labelhandling/ComponentLabelSwitch.java
@@ -1,7 +1,20 @@
 package de.cooperateproject.ui.diff.component.labelhandling;
 
+import de.cooperateproject.modeling.textual.component.cmp.Abstraction;
+import de.cooperateproject.modeling.textual.component.cmp.Attribute;
+import de.cooperateproject.modeling.textual.component.cmp.Class;
 import de.cooperateproject.modeling.textual.component.cmp.Component;
+import de.cooperateproject.modeling.textual.component.cmp.Connector;
+import de.cooperateproject.modeling.textual.component.cmp.Dependency;
+import de.cooperateproject.modeling.textual.component.cmp.Generalization;
+import de.cooperateproject.modeling.textual.component.cmp.Interface;
+import de.cooperateproject.modeling.textual.component.cmp.Manifestation;
+import de.cooperateproject.modeling.textual.component.cmp.Method;
+import de.cooperateproject.modeling.textual.component.cmp.Port;
+import de.cooperateproject.modeling.textual.component.cmp.Provide;
+import de.cooperateproject.modeling.textual.component.cmp.Require;
 import de.cooperateproject.modeling.textual.component.cmp.RootPackage;
+import de.cooperateproject.modeling.textual.component.cmp.Substitution;
 import de.cooperateproject.modeling.textual.component.cmp.util.CmpSwitch;
 
 public class ComponentLabelSwitch extends CmpSwitch<String> {
@@ -17,5 +30,59 @@ public class ComponentLabelSwitch extends CmpSwitch<String> {
     @Override
     public String caseComponent(Component object) {
         return "component " + object.getName();
+    }
+    
+    @Override
+    public String caseClass(Class object) {
+        return "class " + object.getName();
+    }
+    @Override
+    public String caseInterface(Interface object) {
+        return "interface " + object.getName();
+    }
+    @Override
+    public String caseGeneralization(Generalization object) {
+        return String.format("%s isa %s",  object.getLeftClassifier().getName(), object.getRightClassifier().getName());
+    }
+    @Override
+    public String caseAbstraction(Abstraction object) {
+        return String.format("%s abs %s",  object.getLeftClassifier().getName(), object.getRightClassifier().getName());
+    }
+    @Override
+    public String caseManifestation(Manifestation object) {
+        return String.format("%s man %s",  object.getLeftClassifier().getName(), object.getRightClassifier().getName());
+    }
+    @Override
+    public String caseSubstitution(Substitution object) {
+        return String.format("%s sub %s",  object.getLeftClassifier().getName(), object.getRightClassifier().getName());
+    }
+    @Override
+    public String caseDependency(Dependency object) {
+        return String.format("%s dep %s",  object.getLeftClassifier().getName(), object.getRightClassifier().getName());
+    }
+    @Override
+    public String caseRequire(Require object) {
+        return "require " + object.getInterface().getName();
+    }
+    @Override
+    public String caseProvide(Provide object) {
+        return "provide " + object.getInterface().getName();
+    }
+    @Override
+    public String caseConnector(Connector object) {
+        return "connector " + object.getName();
+    }
+    @Override
+    public String casePort(Port object) {
+        return String.format("port %s realizes %s", object.getName(), object.getRealizedClassifier().getName());
+    }
+    
+    @Override
+    public String caseAttribute(Attribute object) {
+        return String.format("%s %s", object.getType().getName(), object.getName());
+    }
+    @Override
+    public String caseMethod(Method object) {
+        return object.getName();
     }
 }
