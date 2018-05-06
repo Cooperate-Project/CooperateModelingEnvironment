d51faaf366bc63a31d6833dcac6083e95ed5c485
sebinside <hi@sebinside.de>
2018-05-06 20:43:19 +0200

    Added custom name resolution for uml activity actions.

--- bundles/de.cooperateproject.modeling.textual.activity/src/de/cooperateproject/modeling/textual/activity/scoping/ActivityDiagramQualifiedNameProvider.java
+++ bundles/de.cooperateproject.modeling.textual.activity/src/de/cooperateproject/modeling/textual/activity/scoping/ActivityDiagramQualifiedNameProvider.java
@@ -1,12 +1,14 @@
 package de.cooperateproject.modeling.textual.activity.scoping;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.uml2.uml.Activity;
+import org.eclipse.uml2.uml.Model;
+import org.eclipse.uml2.uml.OpaqueAction;
 import org.eclipse.xtext.naming.IQualifiedNameConverter;
 import org.eclipse.xtext.naming.QualifiedName;
 
 import com.google.inject.Inject;
 
-import de.cooperateproject.modeling.textual.activity.act.ActionNode;
 import de.cooperateproject.modeling.textual.xtext.runtime.scoping.CooperateQualifiedNameProvider;
 
 public class ActivityDiagramQualifiedNameProvider extends CooperateQualifiedNameProvider {
@@ -16,20 +18,28 @@ public class ActivityDiagramQualifiedNameProvider extends CooperateQualifiedName
 	
 	@Override
 	public QualifiedName getFullyQualifiedName(EObject element) {
-		// FIXME: This is not a real fix of the naming problem, but a workaround for now
-		if (element instanceof ActionNode) {
-			// Fix on instance of the naming problem
-			// OpaqueAction -> Activity -> Model (Naming: "X")
-			// ActionNode -> RootPackage -> ActivityDiagram (Naming: "RootPackage.X")
-			ActionNode action = (ActionNode) element;
-			String name = action.getName();
-			EObject parent = action.eContainer();
-			EObject parentsparent = parent.eContainer();
-			System.out.println(super.getFullyQualifiedName(element));
-			return converter.toQualifiedName(name);
+		if (element instanceof OpaqueAction) {
+			// FIXME: Maybe the problem for this naming thing is elsewhere.
+			// For some reason, these elements are not resolved correctly by the SimpleAttributeResolver from the super implementation.
+			// This forces the correct hierarchy: "RootPackage.Activity.Node" (instead of "Node")
+			return createQualifiedNameForAction((OpaqueAction) element);
 		} else {
 			return super.getFullyQualifiedName(element);
 		}
+	}
+	
+	private QualifiedName createQualifiedNameForAction(OpaqueAction element) {
+		String name = element.getName();
+
+		//TODO: Add support for swimlanes like this: while(element.eContainer() instanceof ...)
+		
+		// Add activity name
+		name = ((Activity) element.eContainer()).getName() + "." + name;
+		
+		// Add model name
+		name = ((Model) element.eContainer().eContainer()).getName() + "." + name;
+		
+		return converter.toQualifiedName(name);
 	}
 
 }
