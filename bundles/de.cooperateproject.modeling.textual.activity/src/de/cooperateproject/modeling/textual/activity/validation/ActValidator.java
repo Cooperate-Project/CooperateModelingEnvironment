1eb48eb2bd6977c6ba1b7ed181d683fc70e6440f
sebinside <hi@sebinside.de>
2018-05-22 15:20:51 +0200

    Added first uml validation.

--- bundles/de.cooperateproject.modeling.textual.activity/src/de/cooperateproject/modeling/textual/activity/validation/ActValidator.java
+++ bundles/de.cooperateproject.modeling.textual.activity/src/de/cooperateproject/modeling/textual/activity/validation/ActValidator.java
@@ -6,12 +6,15 @@ package de.cooperateproject.modeling.textual.activity.validation;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.uml2.uml.ActivityEdge;
 import org.eclipse.xtext.validation.Check;
 
 import com.google.inject.Inject;
 
 import de.cooperateproject.modeling.textual.activity.act.ActPackage;
+import de.cooperateproject.modeling.textual.activity.act.DecisionNode;
 import de.cooperateproject.modeling.textual.activity.act.Flow;
 import de.cooperateproject.modeling.textual.xtext.runtime.issues.IIssueCodeRegistry;
 import de.cooperateproject.modeling.textual.xtext.runtime.validator.ICooperateAutomatedValidator;
@@ -34,11 +37,24 @@ public class ActValidator extends AbstractActValidator {
 
 	@Check
 	private void checkFlowConditionWithMultipleMembers(Flow flow) {
+		// TODO: First, reenable multiflows!
 //		if (flow.getRelatedElements().size() > 2 && flow.getCondition() != null) {
 //			error("Conditions are only allowed for flow between two actions.", flow,
 //					ActPackage.Literals.FLOW__RELATED_ELEMENTS);
 //		}
-		// TODO: Not tested!
+	}
+	
+	@Check
+	private void checkDecitionNodeEdges(DecisionNode node) {
+		EList<ActivityEdge> incomings = node.getReferencedElement().getIncomings();
+		EList<ActivityEdge> outgoings = node.getReferencedElement().getOutgoings();
+		
+		// A decision node has one or two incoming edges and at least one outgoing edge.
+		if(!(incomings != null && incomings.size() >= 1 && incomings.size() <= 2 && outgoings.size() >= 1)) {
+			// FIXME: null?
+			error("A decision node has one or two incoming edges and at least one outgoing edge.", node,
+					null);
+		}
 	}
 	
 	@Override
